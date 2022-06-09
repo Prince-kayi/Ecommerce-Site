89b5eecc83ed972b3f95b2699a9e84cbc7004915
Prince-kayi <84025956+Prince-kayi@users.noreply.github.com>
2022-06-09 18:02:19 +0200

    updated pages

--- src/Pages/Login.js
+++ src/Pages/Login.js
@@ -1,22 +1,43 @@
 import React, { useState } from "react";
 const Login = () => {
+   const [Loginform,setLForm]=useState({
+      emailAddress:" ",
+      password:" ",
+     
+
+  })
+
+  const Changer=(e)=>{
+     const Newdata={...Loginform}
+     Newdata[e.target.name]=e.target.value
+     setLForm(Newdata)
+
+  }
+
+  const Submit=(e)=>{
+     e.preventDefault();
+  }
+
+  const Show=()=>{
+     console.log("LoginForm",Loginform)
+  }
   return(
      <div className="loginn">
        <div className="login-wrap">
-      <form className="login-form">
+      <form className="login-form" onSubmit={Submit}>
                      <h1 className="inn"> Login</h1>
                     <label>Email address
                       <div className="ad">
-                     <input type="text" name="firstName" />
+                     <input type="text" name="emailAddress" onChange={Changer} value={Loginform.emailAddress} required />
                      </div>
                     </label>
                     <label> Password </label>
                        <div className="ad">
-                    <input type="text" name="firstName"/>
+                    <input type="text" name="password" onChange={Changer} value={Loginform.password} required/>
                      </div>    
                     <div className="logi-bottom">
-                   <button className="loggin"> Login</button>
-                    <p>New to BuyAlot ? <a href="/create-ac"> Create account</a></p>
+                   <button className="loggin" onClick={Show}> Login</button>
+                    <p>New to BuyAlot ? <a href="/register"> Create account</a></p>
                </div>
                    
 
--- src/Pages/Register.js
+++ src/Pages/Register.js
@@ -1,121 +1,150 @@
 import React,{useState} from "react";
 const Register=()=>{
+     const [forms,setForms]=useState({
+         firstName:" ",
+         lastName:" ",
+         email: " ",
+         retypeEmail: " ",
+         password: " ",
+         retypePassword: " ",
+         Mnumber:" ",
+         choice: false
+
+     })
+
+     const handlerChange=(e)=>
+     {
+             const{name,value,type,checked}=e.target
+             setForms((forms)=>({
+                     ...forms,
+                     [name]:type=="checkbox"? checked:value
+             }))
+     }
+
+     const ShowResults=()=>{
+         console.log("forms",forms)
+     }
+
+     const onSubmit=(e)=>{
+         e.preventDefault();
+
+     }
     return(
         <>
         <div className="Regi">
             <div className="regi-wrap">
-                <form className="reg-form">
+                <form className="reg-form" onSubmit={onSubmit}>
                     <h1 className="in"> Register</h1>
                     <div className="reg-form-in">
                     <label>first Name*:
-                      <input type="text" name="firstName" />
+                      <input type="text" name="firstName" onChange={handlerChange} value={forms.firstName}  required/>
                     </label>
                     </div>
                     <div  className="reg-form-in">
                     <label>
-                 Last Name*:     <input type="text" name="lastName" />
+                 Last Name*:     <input type="text" name="lastName" onChange={handlerChange} value={forms.lastName} required/>
                     </label>
                     </div>
                     <div className="reg-form-in">
                     <label>
-                 Email*:     <input type="text" name="email" />
+                 Email*:     <input type="text" name="email" onChange={handlerChange} value={forms.email} required/>
                     </label>
                     </div>
                     <div className="reg-form-in">
                     <label>
-                 Retype Email*:     <input type="text" name="retype-email" />
+                 Retype Email*:     <input type="text" name="retypeEmail" onChange={handlerChange} value={forms.retypeEmail} required/>
                     </label>
                     </div>
                     <div className="reg-form-in">
                     <label>
-                 Password*:     <input type="text" name="password" />
+                 Password*:     <input type="text" name="password" onChange={handlerChange}  value={forms.password} required/>
                     </label>
                     </div>
                     <div className="reg-form-in">
                     <label>
-                Retype Password*:     <input type="text" name="retype-password" />
+                Retype Password*:     <input type="text" name="retypePassword" onChange={handlerChange} value={forms.retypePassword} required/>
                     </label>
                     </div>
                     <div className="reg-form-in">
                     <label>
-                Mobile Number*v:     <input type="text" name="number" />
+                Mobile Number*:     <input type="text" name="Mnumber" onChange={handlerChange} value={forms.Mnumber} required />
                     </label>
            </div>
            <div className="boss">
                <div>
                <h4>Sign up to our Newsletters to be the first to know about our great deals!</h4>
                 <div>
-               <input type="checkbox" name="newsletter" /> <span> General Letter</span>
+               <input type="checkbox" name="choice"  onChange={handlerChange}  value={forms.choice}/> <span> General Letter</span>
                </div>
                <div>
-               <input type="checkbox" name="newsletter" /> <span>Electronics</span>
+               <input type="checkbox" name="choice"  onChange={handlerChange} value={forms.choice}/> <span>Electronics</span>
                </div>
                <div>
-               <input type="checkbox" name="newsletter" /><span>Books</span> 
+               <input type="checkbox" name="choice" onChange={handlerChange} value={forms.choice} /><span>Books</span> 
                </div>
                <div>
-               <input type="checkbox" name="newsletter" /><span>Gaming</span> 
+               <input type="checkbox" name="choice" onChange={handlerChange} value={forms.choice}/><span>Gaming</span> 
                </div>
                <div>
-               <input type="checkbox" name="newsletter" /><span>Toys</span> 
+               <input type="checkbox" name="choice" onChange={handlerChange} value={forms.choice}/><span>Toys</span> 
                </div>
                <div>
-               <input type="checkbox" name="newsletter" /><span>Computers & Tablets</span> 
+               <input type="checkbox" name="choice" onChange={handlerChange} value={forms.choice} /><span>Computers & Tablets</span> 
                </div>
                <div>
-               <input type="checkbox" name="newsletter" /><span>Fashion</span> 
+               <input type="checkbox" name="choice" onChange={handlerChange} value={forms.choice}/><span>Fashion</span> 
                </div>
                <div>
-               <input type="checkbox" name="newsletter" /><span>health</span> 
+               <input type="checkbox" name="choice"  onChange={handlerChange} value={forms.choice}/><span>health</span> 
                </div>
                <div>
-               <input type="checkbox" name="newsletter" /><span>Music</span> 
+               <input type="checkbox" name="choice"  onChange={handlerChange} value={forms.choice}/><span>Music</span> 
                </div>
                <div>
-               <input type="checkbox" name="newsletter" /><span>Movies & Series</span> 
+               <input type="checkbox" name="choice"  onChange={handlerChange} value={forms.choice}/><span>Movies & Series</span> 
                </div>
                </div>
               <div className="bos">
               <div>
-               <input type="checkbox" name="newsletter" /><span>Office & Stationary</span> 
+               <input type="checkbox" name="choice"  onChange={handlerChange} value={forms.choice}/><span>Office & Stationary</span> 
                </div>
                <div>
-               <input type="checkbox" name="newsletter" /> <span>Pets</span>
+               <input type="checkbox" name="choice"  onChange={handlerChange} value={forms.choice}/> <span>Pets</span>
                </div>
                <div>
-               <input type="checkbox" name="newsletter" /><span>Tv</span>
+               <input type="checkbox" name="choice"  onChange={handlerChange} value={forms.choice}/><span>Tv</span>
                </div>
                <div>
-               <input type="checkbox" name="newsletter" /> <span>Sports</span> 
+               <input type="checkbox" name="choice"  onChange={handlerChange} value={forms.choice}/> <span>Sports</span> 
                </div>
                <div>
-               <input type="checkbox" name="newsletter" /> <span>Daily Deals</span>
+               <input type="checkbox" name="choice"  onChange={handlerChange} value={forms.choice}/> <span>Daily Deals</span>
                </div>
                <div>
-               <input type="checkbox" name="newsletter" /><span>Camera</span> 
+               <input type="checkbox" name="choice" onChange={handlerChange} value={forms.choice}/><span>Camera</span> 
                </div>
                <div>
-               <input type="checkbox" name="newsletter" /> <span>Baby & Toddlers</span>
+               <input type="checkbox" name="choice"  onChange={handlerChange} value={forms.choice}/> <span>Baby & Toddlers</span>
                </div>
                <div>
-               <input type="checkbox" name="newsletter" /><span>Beauty</span> 
+               <input type="checkbox" name="choice"  onChange={handlerChange} value={forms.choice}/><span>Beauty</span> 
                </div> 
                <div>
-               <input type="checkbox" name="newsletter" /> <span>Auto & DIY</span>
+               <input type="checkbox" name="choice"  onChange={handlerChange} value={forms.choice}/> <span>Auto & DIY</span>
                </div>
                <div>
-               <input type="checkbox" name="newsletter" /><span>Liquor</span> 
+               <input type="checkbox" name="choice" onChange={handlerChange} value={forms.choice} /><span>Liquor</span> 
                </div>
               </div>
                <div className="regi-bottom">
                    <p>You will receive newsletters based on your interests, recent orders and browsing behaviour.</p>
-                   <button className="register-now"> Register Now</button>
+                   <button className="register-now" onClick={ShowResults}> Register Now</button>
                     <p>By clicking on 'Register Now', you agree to our <a href="/terms">Terms and Conditions</a> and confirm that you are over 18 years of age</p>
                </div>
            </div>
                 </form>
             </div>
-            <hi></hi>
+            
         </div>
       </>  
     )
